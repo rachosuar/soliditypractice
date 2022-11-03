14887e8935a33ca5fef8ceb26e37c3ac3c1ccdbe
rachosuar <rachosuar@gmail.com>
2022-11-03 14:17:52 -0300

    correccion

--- 3-argumentyfuncion.sol
+++ 3-argumentyfuncion.sol
@@ -14,22 +14,3 @@ contract FunctionArgument{
  return num;
  }
 }
-
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-
-contract argumentyfuncion {
- 
-    uint private num;
-
-   
-    function set(uint _num) public {
-        num = _num;
-    }
-
-    
-    function get() public view returns (uint) {
-        return num;
-    }
-}
